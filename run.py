c82148b12fc326567764259188181c1860683469
evalshine <dankupr21@gmail.com>
2024-08-17 19:06:26 +0300

    init

--- run.py
+++ run.py
@@ -8,6 +8,8 @@ from app.handlers import router as user_router
 from app.admin import admin as admin_router
 from app.database.models import async_main
 
+from aiogram.client.session.aiohttp import AiohttpSession
+
 
 
 async def main():
@@ -15,7 +17,8 @@ async def main():
     
     await async_main()
     
-    bot = Bot(token=os.getenv('TOKEN'))
+    session = AiohttpSession(proxy='http://proxy.server:3128')
+    bot = Bot(token=os.getenv('TOKEN'), session=session)
     dp = Dispatcher()
     
     dp.include_routers(user_router, admin_router)
